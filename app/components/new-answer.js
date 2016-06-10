eda57c00a599562e5d881644b713dd7f7de42314
SamDurfey <samuel.durfey@gmail.com>
2016-06-10 15:04:38 -0700

    Add new answer form and functionality.
    Answers save correctly to database and list on a question's page, along with their authors

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	answerFormIsShowing: false,
+	actions: {
+		showAnswerForm() {
+			this.set('answerFormIsShowing', true);
+		},
+		saveAnswer() {
+			var params = {
+				author: this.get('author'),
+				answer: this.get('answer'),
+				question: this.get('question')
+			};
+			this.set('answerFormIsShowing', false);
+			this.sendAction('saveAnswer', params);
+		}
+	}
 });
